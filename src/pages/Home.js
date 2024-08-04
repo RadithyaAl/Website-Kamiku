5fb12df9f0b2495658d3a6a9209d399258cafd43
Radithya <alhamdikaradit@gmail.com>
2024-08-04 12:17:13 +0700

    nambahin link prank

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-    <p>hai sayang</p>
+    <div>
+        <p>tes tes tes</p>
+        <p>tes website kamikuuuh</p>
+        <button onClick={() => {window.location.href= "https://youtu.be/dQw4w9WgXcQ?si=Exef5yO_UZgSntW5"}}>klik ini dong</button>
+    </div>
     )
 }
 
