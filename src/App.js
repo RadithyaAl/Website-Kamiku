979158beec9aaf244978b34eb1051e17fc4441eb
Radithya <alhamdikaradit@gmail.com>
2024-08-05 17:27:27 +0700

    menambahkan gambar ke homepage

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Navbar from "./Navbar/Navbar.js";
 function App() {
   return(
   <BrowserRouter>
+  <div className='App'>
   <Navbar/>
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/About' element={<About/>} />
   </Routes>
-  
+  </div>
   </BrowserRouter>
   )
 }
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,23 +3,30 @@ import './Navbar-style.css'
 
 const Navbar = () => {
     const navigate = useNavigate();
-    // Fungsi untuk menangani klik pada gambar
-
+  
     const handleLogoClick = () => {
-        navigate('/'); // Mengarahkan ke halaman Home
+      navigate('/'); // Mengarahkan ke halaman Home
     }
-
+  
     return (
-        <div className='Navigation-Bar'>
-        <ul>
-            <li><img src="/Images/Logo_Kamiku.png" alt="" className='Kamiku_Image' onClick={handleLogoClick} /></li>
+      <div className="Navigation-Bar">
+        <div className="Top-nav-left">
+          <img 
+            src="/Images/Logo_Kamiku.png" 
+            alt="Logo Kamiku" 
+            className="Kamiku_Image" 
+            onClick={handleLogoClick} 
+          />
+          <a className='Tulisan_Kamiku' href=''>KAMIKU</a>
+        </div>
+        <div className="Top-nav-right">
+          <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/About">About</Link></li>
-             
-        </ul>
+          </ul>
         </div>
-
-    )
-}
-
-export default Navbar
+      </div>
+    );
+  }
+  
+export default Navbar;
