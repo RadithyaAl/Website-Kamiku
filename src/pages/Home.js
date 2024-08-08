4c75bd61c8e641743cae93e74f634fb8c6468ff0
Radithya <alhamdikaradit@gmail.com>
2024-08-08 11:02:40 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import '../Page_stylesheets/Home.css';
 
 const Home = () => {
     return (
-    <div className="Home-Container">
+    <>  
+    <div className="Image-Container">
         <img src="/Images/IMG_7197.JPG" alt="" className='kgts-background'/>
         <div className="Img-text-background">
             <div className='img-text'>
@@ -11,6 +12,25 @@ const Home = () => {
             </div>
         </div>
     </div>
+    <div className='Home-MainText'>
+        <h1>Lorem ipsum odor amet, consectetuer adipiscing elit.</h1>
+        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus nascetur hendrerit bibendum consectetur malesuada tincidunt turpis. Praesent lectus commodo curabitur diam nec convallis vestibulum. Fermentum pharetra fames dolor vulputate fames condimentum metus arcu. Proin neque condimentum nec ligula natoque; pretium hendrerit id.</p>
+        <div className='VisiMisi-Container'>
+            <span className='VisiMisi'>
+                <h3>VISI</h3>
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus nascetur hendrerit. sdfsdfskdjfjhkjd</p>
+            </span>
+            <span className='VisiMisi'>
+                <h3>MISI</h3>
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus nascetur hendrerit.</p>
+            </span>
+            <span className='VisiMisi'>
+                <h3>TUJUAN</h3>
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus nascetur hendrerit. consectetuer adipiscing elit. consectetuer adipiscing elit.</p>
+            </span>
+        </div>
+    </div>
+    </>
     )
 }
 
